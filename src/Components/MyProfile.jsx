32db2335386aa1715e778077cad7cdc9fa089af1
Billions94 <E.a.egbedi@gmail.com>
2021-10-12 02:36:23 +0200

    commit

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -22,8 +22,15 @@ const MyProfile = () => {
 
   return (
     <>
+    <div className="mt-3">
+    <a href="https://www.talent.io/p/en-de/home?utm_source=linkedin&utm_medium=cpc&utm_campaign=%5BLI%5D-DE-Germany-Candidates-Frontend-TA&li_fat_id=713b0a02-5b8e-4676-9f0c-592df8135a78" 
+    className="ad text-center text-dark">
+      <a className="text-primary">Frontend Entwickle</a> 
+    🚨  7000 Unternehmen suchen auf talent.io neue Mitarbeiter·innen. €60k to €120k </a>
+    <span className="ml-2">Ad <span className="ad-span">...</span></span>
+    </div>
       {/*Main Container*/}
-      <Container className="mt-5">
+      <Container className="mt-3">
         <Row>
           {/*Larger central Column*/}
           <Col md={8}>
