e35f39deca55fa26e37cf43944b0cf84e4bf6f75
iCarambaaa <dkpress1916@zoho.eu>
2021-10-13 22:12:30 +0200

    done

---
+++ src/Components/MyFeed/PUTModal.jsx
@@ -0,0 +1,87 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState, useEffect } from "react";
+
+const PUTModal = ({ smShow, setSmShow, id }) => {
+  const [text, setText] = useState({ text: "" });
+
+  const updatePost = async (e) => {
+    e.preventDefault(e);
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/${id}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(text),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTYzZjUwMGE4OTBjYzAwMTVjZjA3ZTIiLCJpYXQiOjE2MzM5NDA3MzcsImV4cCI6MTYzNTE1MDMzN30.b4fHuXDwJcxn6c0Vu-wZ1dWzMlTBO6elAUs0C_9xB4o",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log(response);
+        alert("Post successfully");
+      } else {
+        console.log(text);
+
+        console.log(`wow... that wasn't supposed to happen... Error`);
+        alert(`Woops we lost your data in the void .. try refreshing`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    setText(text);
+    console.log(text);
+  }, [text]);
+
+  return (
+    <>
+      <Modal
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">Edit post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="#1">
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="What do you ant to talk about?"
+                // name="description"
+                value={text.text}
+                onInput={(event) =>
+                  setText({ ...text, text: event.target.value })
+                }
+                id="description"
+                rows="4"
+                cols="81"
+              />
+            </Form.Group>
+            <div className="d-flex justify-content-end">
+              <Button
+                variant="primary"
+                type="submit"
+                className="rounded-pill"
+                onClick={(e) => updatePost(e)}
+              >
+                Update
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default PUTModal;
